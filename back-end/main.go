c9edec85ad2eae31a12ca51c19f4d7f7c7625707
TrueHopolok <vadim.truhanov@inbox.lv>
2025-04-08 12:55:29 +0300

    Tidy project, add plog package properly and script to start a server

---
+++ back-end/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	plog "github.com/TrueHopolok/logger--and--spellchecker/plog"
+)
+
+const LOG_FILE_NAME = "back-end/server.log"
+
+func main() {
+	flog, err := os.OpenFile(LOG_FILE_NAME, os.O_WRONLY|os.O_APPEND|os.O_CREATE, 0666)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	defer flog.Close()
+	logger, _ := plog.NewLogger(plog.LevelInfo, flog, plog.RequireTimestamp|plog.RequireLevel, false)
+	logger.Info("program started / logger initializated")
+}
--- go.mod
+++ go.mod
@@ -4,7 +4,10 @@ go 1.24.2
 
 tool golang.org/x/tools/cmd/stringer
 
-require github.com/yuin/gopher-lua v1.1.1
+require (
+	github.com/TrueHopolok/logger--and--spellchecker v0.0.0-20250408094436-751cf4703c0d
+	github.com/yuin/gopher-lua v1.1.1
+)
 
 require (
 	golang.org/x/mod v0.24.0 // indirect
