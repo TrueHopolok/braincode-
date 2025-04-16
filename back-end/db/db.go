6a4517c851a334819e1ed4a1d829ef7e930c1d6e
TrueHopolok <vadim.truhanov@inbox.lv>
2025-04-16 17:25:03 +0300

    bug-fix: provided path to DB file was wrong

--- back-end/db/db.go
+++ back-end/db/db.go
@@ -12,14 +12,14 @@ const DB_FILE_PATH = "back-end/db/db.db"
 
 func Version() {
 	var version string
-	db, _ := sql.Open("sqlite3", "file:db.db")
+	db, _ := sql.Open("sqlite3", fmt.Sprintf("file:%s", DB_FILE_PATH))
 	db.QueryRow(`SELECT sqlite_version()`).Scan(&version)
 	fmt.Println(version)
 }
 
-/* 
+/*
 
-TODO: 
+TODO:
 Database connection, sql query execution.
 In other files all queries are checked and queries prepared for execution.
 Meaning this file only for logic without that much error handling
