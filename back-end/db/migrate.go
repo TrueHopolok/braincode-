cda113d111dc247cdd75add347f87e0cf846ac9e
IwailerI <andrypirk@gmail.com>
2025-04-21 20:56:52 +0300

    added db-version aware migrations

--- back-end/db/migrate.go
+++ back-end/db/migrate.go
@@ -2,13 +2,15 @@ package db
 
 import (
 	"embed"
+	"errors"
 	"fmt"
-	"io"
 	"io/fs"
 	"slices"
 
 	"github.com/TrueHopolok/braincode-/back-end/logger"
 )
+
+const migrationVersionTable = "MigrationVersion"
 
 // migrations contains embedded migration files.
 // This filesystem should only contain sql migration files and directories.
@@ -35,13 +37,40 @@ func Migrate() error {
 
 	slices.Sort(entries)
 
-	for _, entry := range entries {
-		f, err := migrations.Open(entry)
+	var version string
+	{ // lookup database version
+		var dirty bool
+		if err := Conn.QueryRow("SELECT version, dirty FROM "+migrationVersionTable+";").Scan(&version, &dirty); err != nil {
+			// There is no way to check whether a table exists while staying drive-agnostic.
+			// Because of that, we assume that version table does not exist on any failure.
+			version = ""
+			dirty = false
+
+			if _, err := Conn.Exec("CREATE TABLE " + migrationVersionTable + " (version TEXT, dirty BOOLEAN, pk INTEGER PRIMARY KEY);"); err != nil {
+				return fmt.Errorf("migration: cannot create meta table: %w", err)
+			}
+			if _, err := Conn.Exec("INSERT INTO " + migrationVersionTable + " VALUES ('', FALSE, 0);"); err != nil {
+				return fmt.Errorf("migration: cannot create meta table: %w", err)
+			}
+		}
+
+		if dirty {
+			return errors.New("migration: database is dirty, this can happen if something went horribly wrong mid-migration, fix the database manually")
+		}
+	}
+
+	i, found := slices.BinarySearch(entries, version)
+	if !found && version != "" {
+		return fmt.Errorf("migration: database version %v is not known", version)
+	}
+
+	for _, entry := range entries[i:] {
+		data, err := migrations.ReadFile(entry)
 		if err != nil {
 			return fmt.Errorf("migration %s: cannot open embedded file: %w", entry, err)
 		}
 
-		if err := runMigration(f); err != nil {
+		if err := runMigration(entry, data); err != nil {
 			return fmt.Errorf("migration %s: %w", entry, err)
 		}
 	}
@@ -49,23 +78,26 @@ func Migrate() error {
 	return nil
 }
 
-func runMigration(f fs.File) error {
-	stat, err := f.Stat()
+func runMigration(version string, data []byte) error {
+	logger.Log.Debug("Migration: found = %s", version)
+
+	tx, err := Conn.Begin()
 	if err != nil {
-		// Unreachable, embed.FS file State() never fails.
-		panic(fmt.Errorf("migration: cannot stat file: %w", err))
+		return fmt.Errorf("migration %s: cannot start transaction: %w", version, err)
+	}
+	defer tx.Rollback()
+
+	if _, err := tx.Exec("UPDATE " + migrationVersionTable + " SET dirty=TRUE;"); err != nil {
+		return fmt.Errorf("migration %s: cannot write meta table: %w", version, err)
 	}
 
-	logger.Log.Debug("Migration: found = %s", stat.Name())
-	data, err := io.ReadAll(f)
-	if err != nil {
-		// Unreachable, embed.FS file Read() never fails.
-		panic(fmt.Errorf("migration %s: cannot read file: %w", stat.Name(), err))
+	if _, err := tx.Exec(string(data)); err != nil {
+		return fmt.Errorf("migration %s: %w", version, err)
 	}
 
-	if _, err := Conn.Exec(string(data)); err != nil {
-		return fmt.Errorf("migration %s: %w", stat.Name(), err)
+	if _, err := tx.Exec("UPDATE "+migrationVersionTable+" SET dirty=FALSE, version=?;", version); err != nil {
+		return fmt.Errorf("migration %s: cannot write meta table: %w", version, err)
 	}
 
-	return nil
+	return tx.Commit()
 }
