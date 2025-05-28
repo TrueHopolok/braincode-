eb508b80cedfb555d788d4df06a576a6b0ecc5ff
HOPOLOK <vadim.truhanov@inbox.lv>
2025-05-28 14:07:11 +0300

    Added upload task template

--- server/views/task.go
+++ server/views/task.go
@@ -31,3 +31,15 @@ func TaskFindAll(w http.ResponseWriter, username string, isauth, isenglish bool)
 	}
 	return buf.Flush()
 }
+
+// Show the upload task page. Expects all information to be valid.
+func TaskCreate(w http.ResponseWriter, r *http.Request) error {
+	// TODO(vadim): to the finished view add all information
+	templ := "TODO.html" // lang depended
+	buf := bufio.NewWriter(w)
+	err := prepared.Templates.ExecuteTemplate(buf, templ, nil)
+	if err != nil {
+		return err
+	}
+	return buf.Flush()
+}
